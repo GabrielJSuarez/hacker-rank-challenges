40607fc0995cb38b86785ca0f510c39269d9b922
Gabriel J. Suarez <ginnlaiho@gmail.com>
2021-07-14 00:16:31 -0500

    Create breaking_records.rb
    Add code from Breaking records challenge

---
+++ breaking_records.rb
@@ -0,0 +1,27 @@
+def breakingRecords(scores)
+    # Get the lowest and highest score from the Scores Array
+    lowest_season_score = scores[0]
+    highest_season_score = scores[0]
+    
+    # Set the counter for times the records are beaten
+    highest_score_counter = 0
+    lowest_score_counter = 0
+    
+    # loop through the array
+    scores.each do |score|
+        # Check if the score broke the highest record
+        if score > highest_season_score
+            highest_score_counter += 1
+            # re-assing highest record variable
+            highest_season_score = score
+        # Check if the score broke the lowest record  
+        elsif score < lowest_season_score
+            lowest_score_counter += 1
+            # re-assing lowest record varialbe
+            lowest_season_score = score
+        end    
+    end
+    
+    # return results
+    return [highest_score_counter, lowest_score_counter]
+end
